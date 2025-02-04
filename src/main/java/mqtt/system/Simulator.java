f54d841593f38c272216f6206c8ba1a81081eb3e
Bárbara Barsi <bbarsid@gmail.com>
2025-02-04 06:53:39 +0100

    feat: add exercise 4.1

--- src/main/java/mqtt/system/Simulator.java
+++ src/main/java/mqtt/system/Simulator.java
@@ -1,5 +1,6 @@
 package mqtt.system;
 
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import mqtt.entities.Client;
@@ -11,48 +12,37 @@ public class Simulator {
 		String broker = "tcp://localhost:1883"; 
 		String topic = "labs/paho-example-topic";
 	    String messageContent = "Message from my Lab's Paho Mqtt Client";
-	    int qos = 0;     
-	    String clientId  = "myClientID_Pub";
-	    //MemoryPersistence persistence = new MemoryPersistence
+	    int qos = 0;
 		
-		try (
-				Client publisher = new Client(broker, "myClientID_Pub");
-				Client subscriber = new Client(broker, "myClientID_Sub")
-						) {
+		try {
+				
+			Client publisher = new Client(broker, "myClientID_Pub");
+			Client subscriber = new Client(broker, "myClientID_Sub");
 			
-			Thread subscribeThread = new Thread(() -> {
-				try {
-					subscriber.subscribeToTopic(qos, topic);
-				} catch (MqttException e) {
-					e.printStackTrace();
-				}
-			});
+			try{
+		    	MqttConnectOptions connectOptions = new MqttConnectOptions();
+		    	connectOptions.setCleanSession(true);
+		    	
+		    	System.out.println("Connecting " + connectOptions + " to Mqtt Broker running at: " + broker);
+		    	publisher.connect(connectOptions);
+		    	
+		    	
+		    	System.out.println("Connecting " + connectOptions + " to Mqtt Broker running at: " + broker);
+		    	subscriber.connect(connectOptions);
+		    	
+		    	System.out.println("Mqtt Clients sucessfully connected.");
+	        	 
+	         } catch(MqttException e) { Client.printError(e); }	
+		
+			subscriber.subscribeToTopic(qos, topic);
+			subscriber.disconnect();
+			System.out.println("Subscriber disconnected.");
 			
-			subscribeThread.start();
-			
-			Thread publishThread = new Thread(() -> {
-	            try {
-	                for (int i = 1; i <= 5; i++) {
-	                	publisher.publishMessage(topic, messageContent + " Message #" + i, qos);
-	                    Thread.sleep(5000); // Wait 5 seconds between messages
-	                }
-	            } catch (InterruptedException e) {
-	                e.printStackTrace();
-	            }
-	        });
-	        publishThread.start();
-
-	        try {
-	            subscribeThread.join();
-	            publishThread.join();
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
+	        publisher.publishMessage(topic, messageContent + " - q.4.1", qos);
 	        
+	        subscriber.connect();
 			
-		} catch (MqttException e) {
-			e.printStackTrace();
-		}
+		} catch(MqttException e) { Client.printError(e); }	
 		
 		
 		
